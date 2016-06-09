4512e979fa122cfda556a84536bb59d9f1d62f95
sides <sides@sides.tv>
2016-06-09 19:44:42 +0000

    Respect .editorconfig

--- src/TextFormatter/Plugins/Autovideo/Parser.js
+++ src/TextFormatter/Plugins/Autovideo/Parser.js
@@ -3,7 +3,7 @@ var tagName = config.tagName,
 
 matches.forEach(function(m)
 {
-    var tag = addSelfClosingTag(tagName, m[0][1], m[0][0].length);
-    tag.setAttribute(attrName, m[0][0]);
-    tag.setSortPriority(-1);
+  var tag = addSelfClosingTag(tagName, m[0][1], m[0][0].length);
+  tag.setAttribute(attrName, m[0][0]);
+  tag.setSortPriority(-1);
 });
